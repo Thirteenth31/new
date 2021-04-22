fd14a99e9477b9b5cfcca9597a623a6f234826cf
Thirteenth31 <53521548+Thirteenth31@users.noreply.github.com>
2021-04-22 11:56:57 +0000

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,99 @@
+
+$(function(){
+
+    /* Fixed Header */
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+    let burger = $("#burger");
+
+
+    $(window).on("scroll resize",function(){
+
+        introH = intro.innerHeight();
+
+        scrollPos = $(this).scrollTop();
+
+      checkScroll(scrollPos,introH);
+
+    });
+
+
+    function checkScroll(scrollPos,introH){
+
+          if(scrollPos > introH){
+             header.addClass("fixed");
+             navToggle.addClass("visible");
+             nav.addClass("bg");
+
+        }else{
+             header.removeClass("fixed");
+             navToggle.removeClass("visible");
+             nav.removeClass("show");
+             burger.removeClass("x");
+             nav.removeClass("bg");
+        }
+
+    };
+
+
+
+
+    /* Smooth Scroll */
+
+    $("[data-scroll]").on("click",function(event){
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+  nav.removeClass("show");
+  burger.removeClass("x");
+        $("html , body").animate({
+            scrollTop:elementOffset -10
+        },700);
+
+
+
+    });
+
+    /*  Nav Toggle  */
+
+     burger.on("click",function(event){
+        event.preventDefault();
+
+         burger.toggleClass("x");
+
+    });
+
+
+    navToggle.on("click",function(event){
+        event.preventDefault();
+
+        nav.toggleClass("show");
+
+
+
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
